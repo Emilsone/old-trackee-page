fd39021484cfd75223bea3364bf75b78d510f53e
Emilsone <lawsonezekiel68@gmail.com>
2021-05-06 06:21:14 -0700

    rewrite testimonial page

--- src/components/TestimonialThree.vue
+++ src/components/TestimonialThree.vue
@@ -1,16 +1,21 @@
 <template>
-   <div>
+  <div>
     <p class="text-gray-600 px-6 py-6 md:px-16 md:py-10 text-xl md:text-2xl">
-      Bring to the table win-win survival strategies to ensure proactive
-      domination. At the end of the day, going forward, a new normal that has
-      evolved from generation X is on the runway heading towards a streamlined
-      cloud solution. User generated content in real-time will have multiple
-      touchpoints for offshoring.
+      Trackee has helped me manage my team work much better. The intuitive
+      interface makes implementaion very easy and my team loves Trackee.
     </p>
-    <img
-      class="h-full w-full object-cover"
-      src="/user/ben.png"
-      alt="Your avatar"
-    />
+    <div class="px-6 md:px-16">
+      <img
+        class="h-16 w-16 rounded-full"
+        src="/user/edidiong.png"
+        alt="Your avatar"
+      />
+      <h2 class="text-sm md:text-base font-bold text-gray-700 leading-tight">
+        Edidiong David
+      </h2>
+      <small class="text-gray-500 text-xs md:text-sm truncate"
+        >Product Manager</small
+      >
+    </div>
   </div>
 </template>
