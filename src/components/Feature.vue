5f9ad9911a07ed71e48af01a9ade35cfa8c1e64b
Emilsone <lawsonezekiel68@gmail.com>
2021-05-12 14:41:33 -0700

    create feature page

--- src/components/Feature.vue
+++ src/components/Feature.vue
@@ -10,63 +10,74 @@
       </p>
       <div class="flex flex-wrap justify-around md:justify-center">
         <div class="lg:w-1/3 w-5/6 lg:mb-0 mb-6 p-4">
-          <div class="h-full text-center">
+          <feature-page class="h-full text-center">
             <img
               alt="testimonial"
               class="w-20 h-20 mb-8 object-cover object-center inline-block"
               src="../assets/clipboard.png"
             />
-            <h1
+            <template
+              v-slot:headline
               class="text-gray-900 font-bold title-font tracking-wider text-xl"
             >
-              <slot name="headline" />
-            </h1>
-            <p class="leading-relaxed">
-              <slot name="content" />
-            </p>
-          </div>
+              Time Measurement
+            </template>
+            <template v-slot:content class="leading-relaxed">
+              It measures time in a friendly way it;'s a trusted assistant for
+              your team members not a hard task master
+            </template>
+          </feature-page>
         </div>
         <div class="lg:w-1/3 w-5/6 lg:mb-0 mb-6 p-4 md:mx-auto">
-          <div class="h-full text-center">
+          <feature-page class="h-full text-center">
             <img
               alt="testimonial"
               class="w-20 h-20 mb-8 object-cover object-center inline-block"
               src="../assets/easy.png"
             />
-            <h1
+            <template action="Action"
+              v-slot:headline
               class="text-gray-900 font-bold title-font tracking-wider text-xl"
             >
-              <slot name="headline" />
-            </h1>
-            <p class="leading-relaxed">
-             <slot name="content" />
-            </p>
-          </div>
+              Track Progress
+            </template>
+            <template action="Action" v-slot:content class="leading-relaxed">
+              It shows you at what stage you are at and what work is needed to
+              complete a given assignment. Its a motivational aid that helps get
+              things done.
+            </template>
+          </feature-page>
         </div>
         <div class="lg:w-1/3 w-5/6 lg:mb-0 p-4 md:mx-auto">
-          <div class="h-full text-center">
+          <feature-page class="h-full text-center">
             <img
               alt="testimonial"
               class="w-20 h-20 mb-8 object-cover object-center inline-block"
               src="../assets/laugh.png"
             />
-            <h1
+            <template
+              v-slot:headline
               class="text-gray-900 font-bold title-font tracking-wider text-xl"
             >
-              <slot name="headline" />
-            </h1>
-            <p class="leading-relaxed">
-             <slot name="content" />
-            </p>
-          </div>
+              Learning from history
+            </template>
+            <template v-slot:content class="leading-relaxed">
+              When you need to evaluate a new project you can simply tasks and
+              accurately estimate how much effort and it will involve
+            </template>
+          </feature-page>
         </div>
       </div>
     </div>
   </section>
 </template>
 <script>
+import FeaturePage from "@/components/FeaturePage";
 export default {
-  name: "FeaturePage",
+  name: "Feature",
+  components: {
+    "feature-page": FeaturePage,
+  },
 };
 </script>
 <style scoped>
