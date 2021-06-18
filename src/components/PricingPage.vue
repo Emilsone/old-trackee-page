967ace7effd441b4bb3bd345abedac2bfe027dbb
Emilsone <lawsonezekiel68@gmail.com>
2021-06-18 09:30:44 -0700

    add content to pricing page

--- src/components/PricingPage.vue
+++ src/components/PricingPage.vue
@@ -1,10 +1,334 @@
 <template>
-    <main>
-        
-    </main>
+  <main>
+    <div class="bg-white ">
+      <div class="container px-6 py-8 mx-auto">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <div class="py-6">
+            <p class="mt-4 text-gray-500 dark:text-gray-400 uppercase">
+              our plans
+            </p>
+            <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-100">
+              Get Our Reasonable Pricing
+            </h2>
+          </div>
+
+          <div
+            class="
+              overflow-hidden
+              p-0.5
+              mt-6
+            "
+          >
+            <div class="sm:-mx-0.5 flex">
+              <button
+                class="
+                  focus:outline-none
+                  px-6
+                  w-1/2
+                  sm:w-auto
+                  py-2
+                  sm:mx-0.5
+                  text-white
+                  bg-month
+                  
+                "
+              >
+                Monthly
+              </button>
+              <button
+                class="
+                  focus:outline-none
+                  px-6
+                  w-1/2
+                  sm:w-auto
+                  py-2
+                  sm:mx-0.5
+                  text-gray-800
+                  text-black
+                  bg-year
+                  
+                "
+              >
+                Yearly
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          class="
+            flex flex-col
+            items-center
+            justify-center
+            space-y-8
+            lg:-mx-4
+            lg:flex-row
+            lg:items-stretch
+            lg:space-y-0
+          "
+        >
+          <div
+            class="
+              flex flex-col
+              w-full
+              max-w-sm
+              p-8
+              space-y-8
+              text-center
+              bg-white
+              border-2 border-gray-200
+              rounded-lg
+              lg:mx-4
+              dark:bg-gray-800
+              dark:border-gray-700
+            "
+          >
+            <div class="flex-shrink-0">
+              <h2
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  px-2
+                  font-semibold
+                  tracking-tight
+                  heading-text
+                  uppercase
+                  rounded-lg
+                  bg-gray-50
+                  dark:bg-gray-700
+                "
+              >
+                Casual
+              </h2>
+            </div>
+            <div class="flex-shrink-0">
+              <span
+                class="
+                  pt-2
+                  text-4xl
+                  font-bold
+                  text-gray-800
+                  uppercase
+                  dark:text-gray-100
+                "
+              >
+                Free
+              </span>
+            </div>
+            <ul class="flex-1 space-y-4">
+              <li class="text-gray-500 dark:text-gray-400">Up to 5 projects</li>
+              <li class="text-gray-500 dark:text-gray-400">
+                Up to 10 collaborators
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">2Gb of storage</li>
+            </ul>
+
+            <button
+              class="
+                inline-flex
+                items-center
+                justify-center
+                px-4
+                py-2
+                font-semibold
+                btn-text
+                uppercase
+                transition-colors
+                 border-2
+           
+                
+                focus:outline-none
+              "
+            >
+              Start free
+            </button>
+          </div>
+
+          <div
+            class="
+              flex flex-col
+              w-full
+              max-w-sm
+              p-8
+              space-y-8
+              text-center
+              bg-white
+              border-2 border-gray-200
+              rounded-lg
+              lg:mx-4
+              dark:bg-gray-800
+              dark:border-gray-700
+            "
+          >
+            <div class="flex-shrink-0">
+              <h2
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  px-2
+                  font-semibold
+                  tracking-tight
+                  heading-text
+                  uppercase
+                  rounded-lg
+                  bg-gray-50
+                  dark:bg-gray-700
+                "
+              >
+                Profesional
+              </h2>
+            </div>
+            <div class="flex-shrink-0">
+              <span
+                class="
+                  pt-2
+                  text-4xl
+                  font-bold
+                  text-gray-800
+                  uppercase
+                  dark:text-gray-100
+                "
+              >
+                $24.90
+              </span>
+              <span class="text-gray-500 dark:text-gray-400"> /month </span>
+            </div>
+            <ul class="flex-1 space-y-4">
+              <li class="text-gray-500 dark:text-gray-400">
+                Up to 10 projects
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">
+                Up to 20 collaborators
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">10Gb of storage</li>
+              <li class="text-gray-500 dark:text-gray-400">
+                Real-time collaborations
+              </li>
+            </ul>
+
+            <button
+              class="
+                inline-flex
+                items-center
+                justify-center
+                px-4
+                py-2
+                font-semibold
+                btn-text
+                uppercase
+                transition-colors
+                 border-2
+                
+                focus:outline-none
+              "
+            >
+              Start free trial
+            </button>
+          </div>
+
+          <div
+            class="
+              flex flex-col
+              w-full
+              max-w-sm
+              p-8
+              space-y-8
+              text-center
+              bg-white
+              border-2 border-gray-200
+              rounded-lg
+              lg:mx-4
+              dark:bg-gray-800
+              dark:border-gray-700
+            "
+          >
+            <div class="flex-shrink-0">
+              <h2
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  px-2
+                  font-semibold
+                  tracking-tight
+                  heading-text
+                  uppercase
+                    bg-gray-50
+                  dark:bg-gray-700
+                "
+              >
+                Expert
+              </h2>
+            </div>
+            <div class="flex-shrink-0">
+              <span
+                class="
+                  pt-2
+                  text-4xl
+                  font-bold
+                  text-gray-800
+                  uppercase
+                  dark:text-gray-100
+                "
+              >
+                $49.90
+              </span>
+              <span class="text-gray-500 dark:text-gray-400"> /month </span>
+            </div>
+            <ul class="flex-1 space-y-4">
+              <li class="text-gray-500 dark:text-gray-400">
+                Unlimited projects
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">
+                Unlimited collaborators
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">
+                Unlimited storage
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">
+                Real-time collaborations
+              </li>
+              <li class="text-gray-500 dark:text-gray-400">24x7 Support</li>
+            </ul>
+
+            <button
+              class="
+                inline-flex
+                items-center
+                justify-center
+                px-4
+                py-2
+                font-semibold
+                btn-text
+                uppercase
+                transition-colors
+                border-2
+                
+                focus:outline-none
+              "
+            >
+              Start free trial
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 <script>
 export default {
-    name:"PricingPage"
-}
+  name: "PricingPage",
+};
 </script>
+<style>
+.bg-month {
+  background: #0c0e2d;
+}
+.bg-year{
+  background-color:#F2F5FA;
+}
+.heading-text{
+    color: #0c0e2d;
+}
+</style>
