9eec8adfc9e5bb6bae1bad2b5d46bbca9ca14c5b
Emilsone <lawsonezekiel68@gmail.com>
2022-06-30 14:25:51 -0700

    create feature page

--- src/components/mainpage/PricingList.vue
+++ src/components/mainpage/PricingList.vue
@@ -1,116 +1,123 @@
 <template>
-  <div
-    class="
-      flex flex-col
-      items-center
-      justify-center
-      space-y-8
-      lg:-mx-4 lg:flex-row lg:items-stretch lg:space-y-0
-    "
-  >
-    <div
-      class="
-        flex flex-col
-        w-full
-        max-w-sm
-        p-8
-        space-y-8
-        bg-white
-        border-2 border-gray-200
-        lg:mx-4
-        dark:bg-gray-800 dark:border-gray-700
-      "
-    >
-      <router-link to="/register">
-        <a
-          to="/register"
-          class="
-            flex
-            items-center
-            justify-center
-            px-8
-            py-4
-            font-semibold
-            btn-text
-            uppercase
-            transition-colors
-            border-btn
-            focus:outline-none
-          "
-        >
-          Get Started
-        </a>
-      </router-link>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-wrap -m-4">
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <div class="border border-gray-200 p-6">
+            <div
+              class="
+                w-10
+                h-10
+                inline-flex
+                items-center
+                justify-center
+                rounded-full
+                bg-indigo-100
+                text-indigo-500
+                mb-4
+              "
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+              100% free no subscription
+            </h2>
+            <p class="leading-relaxed text-base">
+              Trackee is a free tool you don't need any subscription or credit
+              card detials to get you started.
+            </p>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <div class="border border-gray-200 p-6">
+            <div
+              class="
+                w-10
+                h-10
+                inline-flex
+                items-center
+                justify-center
+                rounded-full
+                bg-indigo-100
+                text-indigo-500
+                mb-4
+              "
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="6" cy="6" r="3"></circle>
+                <circle cx="6" cy="18" r="3"></circle>
+                <path
+                  d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"
+                ></path>
+              </svg>
+            </div>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+              Connect with your favorite tools
+            </h2>
+            <p class="leading-relaxed text-base">
+              One of the ways to boost productivity, is to work with the right
+              tools. You can work with your favorite tool using trackee
+            </p>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <div class="border border-gray-200 p-6">
+            <div
+              class="
+                w-10
+                h-10
+                inline-flex
+                items-center
+                justify-center
+                rounded-full
+                bg-indigo-100
+                text-indigo-500
+                mb-4
+              "
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </div>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+              Limitless Collaboration
+            </h2>
+            <p class="leading-relaxed text-base">
+              Team work brings value to a project, you can collaborate with your
+              other team members using trackee
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div
-      class="
-        flex flex-col
-        w-full
-        max-w-sm
-        p-8
-        space-y-8
-        bg-white
-        border-2 border-gray-200
-        lg:mx-4
-        dark:bg-gray-800 dark:border-gray-700
-      "
-    >
-      <router-link to="/">
-        <a
-          class="
-            flex
-            items-center
-            justify-center
-            px-4
-            py-4
-            font-semibold
-            btn-text
-            uppercase
-            transition-colors
-            border-btn
-            focus:outline-none
-          "
-        >
-          Get started
-        </a>
-      </router-link>
-    </div>
-
-    <div
-      class="
-        flex flex-col
-        w-full
-        max-w-sm
-        p-8
-        space-y-8
-        bg-white
-        border-2 border-gray-200
-        lg:mx-4
-        dark:bg-gray-800 dark:border-gray-700
-      "
-    >
-      <router-link to="/">
-        <a
-          class="
-            flex
-            items-center
-            justify-center
-            px-4
-            py-4
-            font-semibold
-            btn-text
-            uppercase
-            transition-colors
-            border-btn
-            focus:outline-none
-          "
-        >
-          Get started
-        </a>
-      </router-link>
-    </div>
-  </div>
+  </section>
 </template>
 <script>
 export default {
