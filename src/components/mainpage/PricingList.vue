f69dd8ca971001a4408f933c39ebf1c792c3f4fc
Emilsone <lawsonezekiel68@gmail.com>
2022-07-10 12:01:08 -0700

    rewrite fature page

--- src/components/mainpage/PricingList.vue
+++ src/components/mainpage/PricingList.vue
@@ -6,35 +6,24 @@
           <div class="border border-gray-200 p-6">
             <div
               class="
-                w-10
-                h-10
+                w-16
+                h-16
                 inline-flex
                 items-center
                 justify-center
                 rounded-full
-                bg-indigo-100
-                text-indigo-500
+                bg-gray-100
+                text-black
                 mb-4
               "
             >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-6 h-6"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
+              <i class="fa fa-bell-o text-2xl" aria-hidden="true"></i>
             </div>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-              100% free no subscription
+            <h2 class="text-lg text-gray-900 font-bold title-font mb-2">
+              Manage Tasks
             </h2>
             <p class="leading-relaxed text-base">
-              Trackee is a free tool you don't need any subscription or credit
-              card detials to get you started.
+              With Trackee you can easily manage your task, and control productivity
             </p>
           </div>
         </div>
@@ -42,39 +31,24 @@
           <div class="border border-gray-200 p-6">
             <div
               class="
-                w-10
-                h-10
+                w-16
+                h-16
                 inline-flex
                 items-center
                 justify-center
                 rounded-full
-                bg-indigo-100
-                text-indigo-500
+                bg-gray-100
+                text-black
                 mb-4
               "
             >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-6 h-6"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path
-                  d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"
-                ></path>
-              </svg>
+              <i class="fa fa-cog text-2xl" aria-hidden="true"></i>
             </div>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-              Connect with your favorite tools
+            <h2 class="text-lg text-gray-900 font-bold title-font mb-2">
+              Increase Productivity
             </h2>
             <p class="leading-relaxed text-base">
-              One of the ways to boost productivity, is to work with the right
-              tools. You can work with your favorite tool using trackee
+              Trackee helps you to track your progress and measure the time spent in your task.
             </p>
           </div>
         </div>
@@ -82,36 +56,24 @@
           <div class="border border-gray-200 p-6">
             <div
               class="
-                w-10
-                h-10
+                w-16
+                h-16
                 inline-flex
                 items-center
                 justify-center
                 rounded-full
-                bg-indigo-100
-                text-indigo-500
+                bg-gray-100
+                text-black
                 mb-4
               "
             >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-6 h-6"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
+              <i class="fa fa-user-plus text-2xl" aria-hidden="true"></i>
             </div>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-              Limitless Collaboration
+            <h2 class="text-lg text-gray-900 font-bold title-font mb-2">
+              Share Knowledge
             </h2>
             <p class="leading-relaxed text-base">
-              Team work brings value to a project, you can collaborate with your
-              other team members using trackee
+             You can share some articles or guides on how you manage task with Trackee
             </p>
           </div>
         </div>
